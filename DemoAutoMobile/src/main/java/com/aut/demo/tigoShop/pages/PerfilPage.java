4e4063f867e1ec59df3456855ff9cdf154583dc7
Henry <henryalejandromar@gmail.com>
2019-07-23 16:21:11 -0600

    Updates Various
    Actualizaciones Varias

--- DemoAutoMobile/src/main/java/com/aut/demo/tigoShop/pages/PerfilPage.java
+++ DemoAutoMobile/src/main/java/com/aut/demo/tigoShop/pages/PerfilPage.java
@@ -8,6 +8,7 @@ public class PerfilPage extends ToolsPage {
     private static final By BACK = By.id("com.juvomobileinc.tigoshop.sv:id/tbar_back_icon");
     private static final By MY_NUMBER = By.id("com.juvomobileinc.tigoshop.sv:id/profile_number");
     private static final By SUBS_DETAILS = By.id("com.juvomobileinc.tigoshop.sv:id/profile_content_layout");
+    private static final By SUBS_EMPTY = By.id("com.juvomobileinc.tigoshop.sv:id/empty_title");
 
     public PerfilPage() {
     }
@@ -23,9 +24,14 @@ public class PerfilPage extends ToolsPage {
         System.out.println("Su información de Perfil es:");
         String number = getText(MY_NUMBER);
         System.out.println("Su número es "+number);
-        String subs = getText(SUBS_DETAILS);
-        System.out.println("Sus subscripciones:");
-        System.out.println(subs);
+        System.out.println("Subscripciones:");
+        if(exists(SUBS_EMPTY)){
+             String empty = getText(SUBS_EMPTY);
+             System.out.println(empty);
+        } else {
+            String subs = getText(SUBS_DETAILS);
+            System.out.println(subs);
+        }
         return this;
     }
     
