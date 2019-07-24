4530703c38975ae0ab4bf5c44fdec548f27abafd
Henry <henryalejandromar@gmail.com>
2019-07-24 16:20:03 -0600

    Allure annotations
    Anotaciones de Allure

--- DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_002_Show_Balances.java
+++ DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_002_Show_Balances.java
@@ -8,6 +8,8 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Flaky;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 
 @Epic("CU 002 - Show Balances")
@@ -17,31 +19,33 @@ public class CU_002_Show_Balances {
     BalancesSteps balance = new BalancesSteps();
     
     @BeforeEach
-    @Description("Open the Tigo Shop app and start a test")
     @Story("Login to Tigo Shop")
+    @Description("Open the Tigo Shop app and start a test")
+    @Severity(SeverityLevel.BLOCKER)
     void start(){
         balance.start();
     }
     
     @Test
-    @Description("Show all balance information")
     @Story("Show balance information")
+    @Description("Show all balance information")
     @Flaky
     void showBalances01(){
         balance.intoBalancesAndShowInformation();
     }
     
     @Test
-    @Description("Show the information available in the shopping tab")
     @Story("Show balance information in Shopping Tab")
+    @Description("Show the information available in the shopping tab")
     @Flaky
     void showBalances02(){
         balance.showBalanceInStoreTab();
     }
     
     @AfterEach
-    @Description("Close Tigo Shop app")
     @Story("Logout Tigo Shop")
+    @Description("Close Tigo Shop app")
+    @Severity(SeverityLevel.CRITICAL)
     void end(){
         balance.end();
     }
