136803edc198318e1239a8f2dbdd6229c1a7a608
Henry <henryalejandromar@gmail.com>
2019-07-24 18:03:14 -0600

    Change pom.xml
    Cambio en el pom.xml

--- DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_001_Login_Page.java
+++ DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_001_Login_Page.java
@@ -3,15 +3,21 @@ package com.aut.demo.tigoShop.specs;
 import com.aut.demo.tigoShop.steps.HomeSteps;
 import org.junit.jupiter.api.Test;
 import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Flaky;
 import io.qameta.allure.Story;
 
+@Epic("CU 001 - Login Page")
+@Feature("Just enter the app")
 public class CU_001_Login_Page {
     
     HomeSteps step = new HomeSteps();
-    
-    @Description("Just enter the app")
-    @Story("Just enter the app")
+
     @Test
+    @Story("Just enter the app")
+    @Description("Just enter the app")
+    @Flaky
     void inPage(){
         step.start();
         step.inHome();
