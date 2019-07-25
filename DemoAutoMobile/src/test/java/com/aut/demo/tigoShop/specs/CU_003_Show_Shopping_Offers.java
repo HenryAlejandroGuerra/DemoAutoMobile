8a73745aca9c9ab3b832a097483d9e83259e6325
Henry <henryalejandromar@gmail.com>
2019-07-25 09:12:14 -0600

    Change Allure annotations
    Cambios de anotaciones de Allure

--- DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_003_Show_Shopping_Offers.java
+++ DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_003_Show_Shopping_Offers.java
@@ -2,33 +2,43 @@ package com.aut.demo.tigoShop.specs;
 
 import com.aut.demo.tigoShop.steps.ShopSteps;
 import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@Epic("CU 003 - Show Shopping Offers")
+@Feature("Show customer Shopping Offers")
 public class CU_003_Show_Shopping_Offers {
     
     ShopSteps shop = new ShopSteps();
     
     @BeforeEach
-    @Description("Open the Tigo Shop app and start a test")
     @Story("Login to Tigo Shop")
-    public void start(){
+    @Description("Open the Tigo Shop app and start a test")
+    @Severity(SeverityLevel.BLOCKER)
+    void start(){
         shop.start();
     }
 
-    @Description("Show offers and promotions for calls, messages and internet")
-    @Story("Show all offers in all tabs of the shopping tab")
     @Test
-    public void showShoppingOffers01(){
+    @Story("Show all offers in all tabs of the shopping tab")
+    @Description("Show offers and promotions for calls, messages and internet")
+    @Flaky
+    void showShoppingOffers01(){
         shop.showShoppingOffersInAllTab();
     }
 
     @AfterEach
-    @Description("Close Tigo Shop app")
     @Story("Logout Tigo Shop")
-    public void end(){
+    @Description("Close Tigo Shop app")
+    @Severity(SeverityLevel.CRITICAL)
+    void end(){
         shop.end();
     }
     
--- DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_004_Show_Information_Perfil_Tab.java
+++ DemoAutoMobile/src/test/java/com/aut/demo/tigoShop/specs/CU_004_Show_Information_Perfil_Tab.java
@@ -2,32 +2,42 @@ package com.aut.demo.tigoShop.specs;
 
 import com.aut.demo.tigoShop.steps.PerfilSteps;
 import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@Epic("CU 004 - Show Information Perfil Tab")
+@Feature("Show customer Perfil Information Tab")
 public class CU_004_Show_Information_Perfil_Tab {
 
     PerfilSteps perfil = new PerfilSteps();
 
     @BeforeEach
-    @Description("Open the Tigo Shop app and start a test")
     @Story("Login to Tigo Shop")
+    @Description("Open the Tigo Shop app and start a test")
+    @Severity(SeverityLevel.BLOCKER)
     void start(){
         perfil.start();
     }
 
-    @Description("Show the user information from the profile tab")
-    @Story("Show information in Perfil Tab")
     @Test
+    @Story("Show information in Perfil Tab")
+    @Description("Show the user information from the profile tab")
+    @Flaky
     void showShoppingOffers01(){
         perfil.showInformation();
     }
 
     @AfterEach
-    @Description("Close Tigo Shop app")
     @Story("Logout Tigo Shop")
+    @Description("Close Tigo Shop app")
+    @Severity(SeverityLevel.CRITICAL)
     void end(){
         perfil.end();
     }
